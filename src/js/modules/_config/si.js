cba6aad809f704a80bac88986af8ff41068577ca
Rick Mercer <rmercer33@gmail.com>
2020-05-26 23:03:19 -1000

    regenerated si.js

--- src/js/modules/_config/si.js
+++ src/js/modules/_config/si.js
@@ -2,15 +2,17 @@ module.exports = {
   sourceId: 16,
   sid: "pwom",
   prefix: "/t/pwom",
-  books: ["lj","wos","woh","wot"],
-  bookIds: ["xxx","lj","wos","woh","wot"],
+  books: ["lj","wos","woh","wot","early"],
+  bookIds: ["xxx","lj","wos","woh","wot","early"],
 
   contents: {
-    lj: ["xxx","acknow","reader","forwd","intr","chap01","chap02","chap03","chap04","chap05","chap06","chap07","chap08","chap09","chap10","chap11","chap12","eplg"],
-    wos: ["xxx","intr","chap01","chap02","chap03","chap04","aftwrd","eplg","prayer"],
-    woh: ["xxx","advice","preface","l01","l02","l03","l04","l05","l06","l07","l08","l09","l10","l11","l12"],
+    lj: ["xxx","acknow","reader","forwd","intr","chap01","chap02","chap03","chap04","chap05","chap06","chap07","chap08","chap09","chap10","chap11","chap12","eplg","path"],
+    wos: ["xxx","intr","chap01","chap02","chap03","chap04","aftwrd","eplg","prayer","path"],
+    woh: ["xxx","advice","preface","l01","l02","l03","l04","l05","l06","l07","l08","l09","l10","l11","l12","path"],
     woh2: ["xxx","/l01qa","/l02qa","/l06qa","/l07qa","/l08qa","/l09qa","/l10qa","/l11qa","/l12qa"],
     wot: ["xxx","advice","preface","l01","l02","l03","l04","l05","l06","l07","l08","l09","l10","l11","l12","path"],
     wot2: ["xxx","/l01qa","/l06qa","/l07qa","/l09qa","/l11qa"],
+    early: ["xxx","intr","chap01","chap02","chap03","chap04","chap05","chap06","chap07","chap08","chap09","chap10","path"],
+    early2: ["xxx","/chap02qa","/chap03qa","/chap08qa","/chap09qa"],
   }
 };
