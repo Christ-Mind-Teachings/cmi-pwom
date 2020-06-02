03228cce53c026e8a619c4efe4fe744188f85ff4
Rick Mercer <rmercer33@gmail.com>
2020-06-01 22:45:39 -1000

    update config status before pushing

--- src/js/modules/_config/status.js
+++ src/js/modules/_config/status.js
@@ -4,5 +4,5 @@ export const status = {
   woh: "Mon Jun  1 22:27:34 HST 2020",
   wot: "Sat May 23 10:16:42 HST 2020",
   wok: "Sun May 31 12:19:09 HST 2020",
-  early: "Tue May 26 22:56:53 HST 2020"
+  early: "Mon Jun  1 22:39:18 HST 2020"
 };
